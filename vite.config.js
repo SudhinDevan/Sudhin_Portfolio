77163f67ff717437bab44c183c0497f21013bcef
sudhin devan <sudhindevan@gmail.com>
2023-11-03 16:15:22 +0530

    deploy 1.0

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/3D-Threejs-Portfolio",
+  base: "/Sudhin_Portfolio/",
   plugins: [react()],
-  server: {
-    host: true,
-  },
+  // server: {
+  //   host: true,
+  // },
 });
